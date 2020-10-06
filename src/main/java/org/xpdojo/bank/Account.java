d01c9a865a024cbb2f20b791a44afcf6dfc18d90
Piyush Vaishnav <piyush@piyushs-air.home>
2020-10-06 14:16:10 +0100

    basic withdraw

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -4,11 +4,18 @@ public class Account {
 
     private int balance  = 0;
 
+    public static Account emptyAccount() {
+        return new Account();
+    }
     public int balance() {
         return balance;
     }
 
     public void deposit(int amount) {
         this.balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        this.balance -= amount;
     }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,25 +8,32 @@ public class AccountTest {
 
     @Test
     public void newAccountShouldHave0Balance() {
-
-        Account acc = new Account();
+        Account acc = Account.emptyAccount();
         assertThat(acc.balance()).isEqualTo(0);
-
     }
 
     @Test
     public void depositingAmountShouldIncreaseBalance() {
-        Account acc = new Account();
+        Account acc = Account.emptyAccount();
         acc.deposit(10);
         assertThat(acc.balance()).isEqualTo(10);
     }
 
     @Test
     public void multipleDepositsShouldAddUpInBalance() {
-        Account acc = new Account();
+        Account acc = Account.emptyAccount();
         acc.deposit(10);
         acc.deposit(20);
         assertThat(acc.balance()).isEqualTo(30);
     }
+
+    @Test
+    public void withdrawFromAccountReflectsInBalance() {
+        Account acc = Account.emptyAccount();
+        acc.deposit(10);
+        acc.withdraw(8);
+        assertThat(acc.balance()).isEqualTo(2);
+    }
+    
 
 }
